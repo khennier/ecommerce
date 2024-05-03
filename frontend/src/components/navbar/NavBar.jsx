67874609f031caae1e9662a211151ec4b6b5630a
khennier <78939009+khennier@users.noreply.github.com>
2024-05-02 23:19:33 -0300

    Agregando funciones al navbar

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './NavBar.css'
-
+import logo from '../assets/logo.png'
+import cart_icon from '../assets/cart_icon.png'
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={logo} alt="" />
+            <p>SHOPPER</p>
+        </div>
+        <ul className="nav-menu">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
   )
 }
