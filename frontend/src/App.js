9543f9386e5fc9f8bbaae13b38619c2b06f462a6
khennier <78939009+khennier@users.noreply.github.com>
2024-05-05 12:10:38 -0300

    Actualizando paginas y funciones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 
 import './App.css';
 import { NavBar } from './components/navbar/NavBar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Shop from './pages/Shop'
+
 
 function App() {
   return (
     <div>
-      <NavBar/>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}></Route>
 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
 import logo from '../assets/logo.png'
 import cart_icon from '../assets/cart_icon.png'
 export const NavBar = () => {
-  return (
+    const [menu, setMenu] = useState ("tienda");
+      return (
     <div className='navbar'>
         <div className='nav-logo'>
             <img src={logo} alt="" />
-            <p>SHOPPER</p>
+            <p>ECOMMERCE</p>
         </div>
         <ul className="nav-menu">
-            <li>Shop</li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick={()=>{setMenu("tienda")}}>Tienda{menu==="tienda"?<hr />:<></>}</li>
+            <li onClick={()=>{setMenu("niños")}}>Niños{menu==="niños"?<hr />:<></>}</li>
+            <li onClick={()=>{setMenu("niñas")}}>Niñas{menu==="niñas"?<hr />:<></>}</li>
+            <li onClick={()=>{setMenu("accesorios")}}>Accesorios{menu==="accesorios"?<hr />:<></>}</li>
         </ul>
-        <div className="nav-login-cart"></div>
+        <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+        </div>
     </div>
   )
 }
