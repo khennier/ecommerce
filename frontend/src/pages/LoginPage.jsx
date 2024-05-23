c55e074eb09cc5fe5daea04ea89aae7e9edfba64
khennier <78939009+khennier@users.noreply.github.com>
2024-05-22 23:27:52 -0300

    actualizado

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './Css/Loginpage.css';
 
 export const LoginPage = () => {
   return (
@@ -6,21 +7,20 @@ export const LoginPage = () => {
       <div className="loginpage-container">
         <h1>Registrate</h1>
         <div className="loginpage-fields">
-          <input type="text" placeholder='Ingresa tu nombre' name="" id="" />
-          <input type="Email" placeholder='Ingresa tu Email' />
-          <input type="password" placeholder='Ingresa tu contraseña' />
+          <input type="text" placeholder='Ingresa tu nombre' name="name" />
+          <input type="email" placeholder='Ingresa tu Email' name="email" />
+          <input type="password" placeholder='Ingresa tu contraseña' name="password" />
           <button>Continuar</button>
-
+          
           <p className="loginpage-login">
-            Ya tienes una cuenta ? <span>Logeate aqui</span>
+            Ya tienes una cuenta? <span>Logeate aqui</span>
           </p>
           <div className="loginpage-agree">
-            <input type="checkbox" name="" id="" />
-            <p>Para continuar, acepto los terminos, condiciones y politicas de privacidad</p>
+            <input type="checkbox" name="terms" id="terms" />
+            <p>Para continuar, acepto los términos, condiciones y políticas de privacidad</p>
           </div>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom'
 
 export const Product = () => {
+  const {all_product}= useContext(ShopContext)
+  const {productId}= useParams()
   return (
     <div>product</div>
   )
