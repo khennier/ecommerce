00991c9640bae2477269d33f24f44fc620631c82
khennier <78939009+khennier@users.noreply.github.com>
2024-05-27 18:26:49 -0300

    Actualizando panel admin y finalizando apis

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import nav_logo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src={nav_logo} alt="" className='nav-logo' />
+        <img src={navProfile} alt="" className='nav-profile'/>
+    </div>
+  )
+}
